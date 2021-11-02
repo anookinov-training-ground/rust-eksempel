5dd6f3b643eec6b29f14645a6d39b1aa749fbada
Alexander Nookinov <anookinov@gmail.com>
2021-11-02 15:31:06 +0700

    add strlen functions example to show monomorphization which is rust code generation during compile time per generice types coded

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn strlen(s: impl AsRef<str>) -> usize {
+    s.as_ref().len()
+}
+
+pub fn strlen_refstr(s: &str) -> usize {
+    s.len()
+}
+
+pub fn strlen_string(s: String) -> usize {
+    s.len()
+}
+
+pub fn strlen2<S>(s: S) -> usize
+where
+    S: AsRef<str>,
+{
+    s.as_ref().len()
+}
+
+pub fn foo() {
+    strlen("hello world"); // &'static str
+    strlen(String::from("hei verden")); // String: AsRef<str>
 }
