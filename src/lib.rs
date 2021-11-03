5c5c939030740953e10e5105a85b361dbafb1c99
Alexander Nookinov <anookinov@gmail.com>
2021-11-03 11:48:52 +0700

    add comment related to vtable

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,37 @@ where
     s.as_ref().len()
 }
 
+pub fn strlen3<S: AsRef<str>>(s: S) -> usize {
+    s.as_ref().len()
+}
+
+pub fn strlen_dyn2(s: Box<dyn AsRef<str>>) -> usize {
+    s.as_ref().as_ref().len()
+}
+
+pub fn strlen_dyn(s: &dyn AsRef<str>) -> usize {
+    s.as_ref().len()
+}
+
+pub fn main() {
+    let x = Box::new(String::from("hello"));
+    let y: Box<dyn AsRef<str>> = x;
+    strlen_dyn2(y);
+
+    let z: Box<dyn AsRef<str>> = Box::new(String::from("hello"));
+    strlen_dyn2(z);
+
+    let a: &dyn AsRef<str> = &"world";
+    strlen_dyn(a);
+
+    let random = 4; // read from the user
+    if random == 4 {
+        say_hei(&"hello");
+    } else {
+        say_hei(&String::from("world"));
+    }
+}
+
 pub fn bool_then<T>(b: bool, f: impl FnOnce() -> T) -> Option<T> {
     if b {
         Some(f())
@@ -40,6 +71,48 @@ impl Hei for String {
         println!("hei {}", self);
     }
 }
+
+pub fn say_hei_static<H: Hei>(s: H) {
+    s.hei();
+}
+
+pub fn say_hei_static_str(s: &str) {
+    s.hei(); // call compiled static assembly code
+}
+
+pub fn say_hei(s: &dyn Hei) {
+    // &dyn Hei
+    // stored in &
+    //  1. a pointer to the actual, concrete, implementing type
+    //  2. a pointer to a vtable for the referenced trait
+    //
+    // what is a vtable (virtual dispatch table)?
+    // dyn Hei, vtable:
+    //  struct HeiVtable {
+    //    hei: *mut Fn(*mut ()),
+    //  }
+    //
+    // &str -> &dyn Hei
+    //
+    // 1. pointer to the &str
+    // 2. HeiVtable {
+    //   hei: &<str as Hei>::hei
+    // }
+    // String -> &dyn Hei
+    //
+    // 1. pointer to the String
+    // 2. HeiVtable {
+    //   hei: &<String as Hei>::hei
+    // }
+    s.hei();
+    // s.vtable.hei(s.pointer)
+}
+
+// struct Foo {
+//     s: [u8],
+// }
+
+// pub fn foo_dyn(h: Hei) {}
 
 pub fn foo() {
     strlen("hello world"); // &'static str
@@ -72,3 +145,9 @@ pub fn bar_slice(s: &[impl Hei]) {
         h.hei();
     }
 }
+
+// pub fn bar_dyn(s: &[dyn Hei]) {
+//     for h in s {
+//         h.hei();
+//     }
+// }
